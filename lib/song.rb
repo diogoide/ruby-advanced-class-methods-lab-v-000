b93c40ae7008c7a23932a809f790b150f8ee58e0
Diogo Ide <diogoide@gmail.com>
2018-04-22 16:19:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
     split_filename = filename.split(" - ")
     artist_name = split_filename[0]
     binding.pry
-    name = split_filename[1].split(".").pop
+    name[0] = split_filename[1].split(".").pop
     name = name[0]
   end
 
